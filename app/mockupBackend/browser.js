83ccdd4502883cc079828a6749012e561e5be0d0
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2024-01-02 20:58:54 +0400

    change next

--- app/mockupBackend/browser.js
+++
@@ -1,7 +0,0 @@
-import { handlers } from './handlers'
-let worker = null;
-if (typeof window !== 'undefined') {
-    const { setupWorker } = await import('msw/browser');
-    worker = setupWorker(...handlers)
-}
-export default worker
